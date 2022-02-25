87b4e7ec62c8ed95cfdd767e7525a5da11ebb458
Grant Moore <gmoore@makpar.com>
2022-02-25 15:15:42 -0500

    modifying styles, add material imports

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button'; 
 import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon'; 
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule
+
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
