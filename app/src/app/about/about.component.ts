1980b96278b602f13f6705e92e77c81273353f13
Grant Moore <gmoore@makpar.com>
2022-02-26 16:58:10 -0500

    wiring animations in about component

--- app/src/app/about/about.component.ts
+++ app/src/app/about/about.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { AnimationControl, Animations, AnimationTriggers, ExpandStates } from 'src/animations';
+import { AnimationControl, Animations, AnimationTriggers, ExpandStates, HighlightStates } from 'src/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
   animations: [
-    Animations.getManualExpandTrigger('60%', '80%')
+    Animations.getManualExpandTrigger('60%', '80%'),
+    Animations.getManualHighlightTrigger(1.25)
   ]
 })
 export class AboutComponent implements OnInit {
 
   public popupExpandCntl = new AnimationControl(AnimationTriggers.cntl_expand);
+  public factHighlightCntls : AnimationControl[] = [
+    new AnimationControl(AnimationTriggers.cntl_highlight),
+    new AnimationControl(AnimationTriggers.cntl_highlight),
+    new AnimationControl(AnimationTriggers.cntl_highlight),
+    new AnimationControl(AnimationTriggers.cntl_highlight),
+  ]
 
   constructor() { 
     this.popupExpandCntl.setState(ExpandStates.closed);
@@ -32,4 +39,42 @@ export class AboutComponent implements OnInit {
     return this.popupExpandCntl.state == ExpandStates.closed
   }
 
+  public highlight(factIndex: number){
+    this.factHighlightCntls[factIndex].animate()
+  }
+
+  public delight(factIndex: number){
+    this.factHighlightCntls[factIndex].prime()
+  }
+
+  public getMessage(factIndex: number){
+    if(this.factHighlightCntls[factIndex].state == HighlightStates.normal){
+      switch(factIndex){
+        case 0:
+          return "Experience";
+        case 1:
+          return "Certifications";
+        case 2:
+          return "Education";
+        case 3:
+          return "Portfolio";
+        default:
+          return "";
+      }
+    }
+    else{
+      switch(factIndex){
+        case 0:
+          return "A wealth of technical expertise";
+        case 1:
+          return "Industry recognized quality";
+        case 2:
+          return "Diverse, multidisciplined background";
+        case 3:
+          return "Websites, applications & projects";
+        default:
+          return "";
+      }
+    }
+  }
 }
