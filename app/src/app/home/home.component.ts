3c95c36ce1444c7a511509a6dc1e747201281637
Grant Moore <chinchalinchin@gmail.com>
2022-03-06 09:43:37 -0500

    updating home content

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -30,16 +30,16 @@ enum States{
                                         { top: '85%', left: '72.5%' },
                                         'cloud_btn_4', AnimationPeriods.short),
                                         
-    Animations.getManualPositionTrigger({ top: '35%', left: '12.5%' }, 
+    Animations.getManualPositionTrigger({ top: '35%', left: '7.5%' }, 
                                         { top: '86%', left: '77.5%', right: '0%' },
                                         'cloud_line_1', AnimationPeriods.short),
-    Animations.getManualPositionTrigger({ top: '55%', left: '17.5%' }, 
+    Animations.getManualPositionTrigger({ top: '55%', left: '12.5%' }, 
                                         { top: '66%', left: '77.5%', right: '0%' },
                                         'cloud_line_2', AnimationPeriods.short),
-    Animations.getManualPositionTrigger({ top: '35%', right: '12.5%' }, 
+    Animations.getManualPositionTrigger({ top: '35%', right: '7.5%' }, 
                                         { top: '46%', left: '77.5%', right: '0%' },
                                         'cloud_line_3', AnimationPeriods.short),
-    Animations.getManualPositionTrigger({ top: '55%', right: '17.5%' },  
+    Animations.getManualPositionTrigger({ top: '55%', right: '12.5%' },  
                                         { top: '26%', left: '77.5%', right: '0%'},
                                         'cloud_line_4', AnimationPeriods.short),
 
@@ -49,15 +49,16 @@ enum States{
 export class HomeComponent implements OnInit {
 
   public displayLines: boolean = true;
-  
+  public selecting: boolean = false;
   public moved: boolean = false;
   public animated: boolean = false;
   public states = States;
   public state = States.one;
   public centerPositionCntl: AnimationControl = new AnimationControl(AnimationTriggers.cntl_position);
   public centerFadeCntl: AnimationControl = new AnimationControl(AnimationTriggers.cntl_fade);
-  public selectorPositionCntl: AnimationControl = new AnimationControl(AnimationTriggers.cntl_position);
+  public selectionPositionCntl: AnimationControl = new AnimationControl(AnimationTriggers.cntl_position);
   public selectorFadeCntl: AnimationControl = new AnimationControl(AnimationTriggers.cntl_fade);
+  public selectionFadeCntl: AnimationControl = new AnimationControl(AnimationTriggers.cntl_fade);
   public cloudFadeCntl: AnimationControl = new AnimationControl(AnimationTriggers.cntl_fade);
   public cloudBtnPositionCntls: AnimationControl[] = [
     new AnimationControl(AnimationTriggers.cntl_position),
@@ -89,13 +90,11 @@ export class HomeComponent implements OnInit {
 
   public display(){
     this.centerPositionCntl.animate();
-    this.selectorPositionCntl.animate();
+    this.selectionPositionCntl.animate();
     this.cloudLinePositionCntls.forEach((cntl: AnimationControl)=>{ cntl.animate(); }); 
-    
     setTimeout(()=>{
       this.moved = true;
     }, AnimationPeriods.short*500);
-
     setTimeout(()=>{
       this.centerFadeCntl.animate();
       this.cloudFadeCntl.prime();
@@ -109,7 +108,13 @@ export class HomeComponent implements OnInit {
   }
 
   public setState(state: States){
-    this.state = state; 
+    this.selectionFadeCntl.animate();
+    this.selecting = true;
+    setTimeout(()=>{
+      this.state = state; 
+      this.selecting = false;
+      this.selectionFadeCntl.prime();
+    }, AnimationPeriods.short*1000)
   }
 
   public selected(state: States){
@@ -140,7 +145,7 @@ export class HomeComponent implements OnInit {
       case this.states.three:
           return "Professional Tier Solutions";
       case this.states.four:
-          return "";
+          return "Creative Design";
       default:
           return "";
     }
@@ -153,7 +158,22 @@ export class HomeComponent implements OnInit {
       case this.states.two:
           return "Bringing the cloud's economy of scale to small businesses";
       case this.states.three:
-          return "Drawing on years of production web and software development";
+          return "Drawing on years of production web development";
+      case this.states.four:
+          return ""
+      default:
+          return "";
+    }
+  }
+
+  public getBlurb(): string{
+    switch(this.state){
+      case this.states.one:
+          return "The <strong>Cumberland Cloud</strong> offers custom web page design and application development for small business owners looking to expand their digital reach without breaking the bank. No matter what your vision for your new website looks like, whether it includes an online store, a reservation system, a forum for users or anything else, we can help you bring to it life while staying within budget.";
+      case this.states.two:
+          return "The cloud provides a serverless, low-maintenance environment for the modern web application. The <strong>Cumberland Cloud</strong> specializes in using this pioneering technology to architect cost-optimized solutions. As a result, our sites are not only more secure, more scalable and more easily deployed than what the competition offers, but they are cheaper too!";
+      case this.states.three:
+          return "Our team has worked on production-scale projects with leading names in the industry. They bring to each project a professional approach, honed with years of experience. Every website we produce is hand-crafted and built from scratch. You won't find a site anywhere else on the internet that look like ours. ";
       case this.states.four:
           return ""
       default:
@@ -164,7 +184,7 @@ export class HomeComponent implements OnInit {
   public getLine(state: States): string{
     switch(state){
       case this.states.four:
-        if(this.moved) return "Pioneering";
+        if(this.moved) return "Innovative";
         return "Give me a page to land";
       case this.states.three:
         if(this.moved) return "Proven";
@@ -173,8 +193,8 @@ export class HomeComponent implements OnInit {
         if(this.moved) return "Affordable";
         return "And I will move the world.";
       case this.states.one:
-        if(this.moved) return "Innovative";
-        return "- <a href=\"https://www.goodreads.com/quotes/16830-give-me-a-place-to-stand-and-a-lever-long\" target=\"_blank\" rel=\"noopener noreferrer\">Archimedes, probably</a>";
+        if(this.moved) return "Guaranteed";
+        return "<a href=\"https://www.cs.drexel.edu/~crorres/Archimedes/Lever/LeverQuotes_OLD.html\" target=\"_blank\" rel=\"noopener noreferrer\">- Archimedes, probably</a>";
       default:
         return ""
     }
