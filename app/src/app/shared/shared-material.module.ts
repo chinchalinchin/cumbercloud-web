273d3249105fed9397323ae487e37d7af388c01c
Grant Moore <chinchalinchin@gmail.com>
2022-05-01 13:01:01 -0400

    fix module

--- app/src/app/shared/shared-material.module.ts
+++ app/src/app/shared/shared-material.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
   imports: [MatIconModule],
   exports: [MatIconModule],
 })
