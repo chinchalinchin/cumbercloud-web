dec6e02f14b1f390272e8fe52e1856acfe1d4584
Grant Moore <chinchalinchin@gmail.com>
2022-05-08 02:53:47 -0400

    update assets

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -818,9 +818,21 @@ export const ASSET_CONFIG: ImgConfig[] = [
   {
     src: '/assets/imgs/innolab.png',
     alt: 'Innovation Lab',
-    title: 'A Shining Sun',
-    id: 'svg-sun-graphic',
+    title: 'Makpar, Innovation Lab',
+    id: 'innolab-img',
   },
+  {
+    src: '/assets/imgs/gsa.png',
+    alt: 'Buy@GSA Homepage',
+    title: 'Buy@GSA Homepage',
+    id: 'gsa-img'
+  },
+  {
+    src: '/assets/imgs/scrilla.png',
+    alt: 'Scrilla Documentation Homepage',
+    title: 'Scrilla: Financial Analysis',
+    id: 'scrilla-img'
+  }
 ];
 
 export const TOOL_CHIPS: ChipConfig[] = [
